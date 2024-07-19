cc50b01179b6f65907cec15967f4b3cb98a075e1
miruware <miruware@worker01.cluster.local>
2024-07-19 11:14:03 +0900

    ansible sudo pass 추가

--- astrago_gui_installer.py
+++ astrago_gui_installer.py
@@ -129,7 +129,7 @@ class CommandRunner:
                                   "--become", "--become-user=root",
                                   "cluster.yml",
                                   "--extra-vars",
-                                  "ansible_user={} ansible_password={}".format(username, password)],
+                                  "ansible_user={} ansible_password={} ansible_sudo_pass={}".format(username, password, password)],
                                  cwd='kubespray')
 
     def _run_command(self, cmd, cwd="."):
