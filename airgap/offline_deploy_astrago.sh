11b1aae3d922760531fa621457979b6865efd8ac
root <root@worker.cluster.local>
2024-08-28 07:38:22 +0000

    airgap update

--- airgap/offline_deploy_astrago.sh
+++ airgap/offline_deploy_astrago.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
 
+CURRENT_DIR=$(dirname "$(realpath "$0")")/..
+
 # Fixing the environment name
 environment_name="astrago"
-
-CURRENT_DIR=$(dirname "$(realpath "$0")")
 
 # Function to check and install binaries
 install_binary() {
@@ -16,7 +16,7 @@ install_binary() {
             sudo chmod +x /usr/local/bin/$cmd
         else
             echo "FATAL: $cmd binary not found in tools folder."
-            exit 1
+	    exit 1
         fi
     else
         echo "$cmd is already installed."
@@ -68,21 +68,6 @@ get_ip_address() {
     eval "$ip_variable=$ip"
 }
 
-# Function to get the volume type from the user
-get_volume_type() {
-    local volume_type_variable=$1
-    while true; do
-        echo -n "Enter the volume type (nfs or local): "
-        read -r volume_type
-        if [ "$volume_type" == "nfs" ] || [ "$volume_type" == "local" ]; then
-            eval "$volume_type_variable=$volume_type"
-            break
-        else
-            echo "Invalid volume type. Please enter again."
-        fi
-    done
-}
-
 # Function to get the base path for folder creation from the user
 get_base_path() {
     local base_path_variable=$1
@@ -92,13 +77,29 @@ get_base_path() {
     eval "$base_path_variable=$base_path"
 }
 
+# Function to get offline registry and HTTP server from the user
+get_offline_settings() {
+    local registry_variable=$1
+    local http_server_variable=$2
+
+    echo -n "Enter the offline registry (e.g. 10.61.3.8:35000): "
+    read -r registry
+    eval "$registry_variable=$registry"
+
+    echo -n "Enter the HTTP server (e.g. http://10.61.3.8): "
+    read -r http_server
+    eval "$http_server_variable=$http_server"
+}
+
 # Main function
 main() {
+    cd $CURRENT_DIR
     case "$1" in
         "--help")
             print_usage
             ;;
         "env")
+            create_environment_directory
             # Get the external IP address from the user
             get_ip_address external_ip "Enter the connection URL (e.g. 10.61.3.12)"
 
@@ -110,14 +111,21 @@ main() {
 
             values_file="environments/$environment_name/values.yaml"
 
-            create_environment_directory
             # Modify externalIP
             yq -i ".externalIP = \"$external_ip\"" "$values_file"
 
             # Modify nfs server IP address and base path
-            yq -i ".nfs.server = \"$nfs_server_ip\"" "$values_file"
+            yq -i ".nfs.enabled = true" "$values_file"
             yq -i ".nfs.server = \"$nfs_server_ip\"" "$values_file"
             yq -i ".nfs.basePath = \"$nfs_base_path\"" "$values_file"
+
+            # Get the offline registry and HTTP server from the user
+            get_offline_settings offline_registry offline_http_server
+
+            # Modify offline settings
+            yq -i ".offline.registry = \"$offline_registry\"" "$values_file"
+            yq -i ".offline.httpServer = \"$offline_http_server\"" "$values_file"
+
             echo "values.yaml file has been modified."
             ;;
         "sync" | "destroy")
@@ -142,4 +150,3 @@ for cmd in helm helmfile kubectl; do
     install_binary $cmd
 done
 main "$@"
-
